c1cc500a7596c916e89c2cf863287bac76a384b2
aitomo <t_aihara19971217@icloud.com>
2024-09-03 23:26:53 +0900

    プロジェクト登録

---
+++ src/main/java/com/example/joymic/JoymicBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.joymic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoymicBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoymicBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/joymic/JoymicBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joymic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoymicBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
